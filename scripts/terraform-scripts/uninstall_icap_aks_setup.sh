94efd07b99564d858d1975d254447eeac32ee394
samarthdd <samarth@smalldaytech.com>
2021-02-04 15:25:07 +0530

    updated deletion of service principle

--- scripts/terraform-scripts/uninstall_icap_aks_setup.sh
+++ scripts/terraform-scripts/uninstall_icap_aks_setup.sh
@@ -3,10 +3,17 @@
 #terraform destroy -auto-approve
 terraform destroy
 
+@deletes keyvault
 az keyvault delete --name $VAULT_NAME --resource-group $RESOURCE_GROUP_NAME
 
+#deletes container
 az storage container delete --account-key $ACCOUNT_KEY --account-name $STORAGE_ACCOUNT_NAME --name $CONTAINER_NAME
 
+#deletes storage account
 az storage account delete -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME
 
+#deletes resource
 az group delete -n $RESOURCE_GROUP_NAME
+
+#deletes service priniple
+az ad sp delete --id $appID
