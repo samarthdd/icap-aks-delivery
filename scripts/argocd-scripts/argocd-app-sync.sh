de38bb67ddecd663fdf6d385e6d844ffbd5a955f
samarthdd <samarth@smalldaytech.com>
2021-02-09 16:27:40 +0530

    app sync script

--- scripts/argocd-scripts/argocd-app-sync.sh
+++ scripts/argocd-scripts/argocd-app-sync.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 #list of apps
+
+# App Name
+ADAPTATION_SERVICE="icap-adaptation-service"
+ADMINISTRATION_SERVICE="icap-administration-service"
+NCFS_SERVICE="icap-ncfs-service"
+RABBITMQ_OPERATOR="rabbitmq-operator"
+MONITORING_SERVICE="monitoring"
+CERT_MANAGER="cert-manager"
+
+argocd app sync $RABBITMQ_OPERATOR-ukw-develop
+
+argocd app sync $CERT_MANAGER-ukw-develop
+
 app_list=$(argocd app list --output name)
 
 for item in $app_list
@@ -9,13 +22,13 @@ do
 done
 
 
-#argocd app sync RABBITMQ_OPERATOR
-#argocd app sync ADAPTATION_SERVICE
-#argocd app sync ADMINISTRATION_SERVICE
-#argocd app sync NCFS_SERVICE
-#argocd app sync MONITORING_SERVICE
-#argocd app sync MONITORING_SERVICE_GRAFANA
-#argocd app sync CERT_MANAGER
+#argocd app sync $RABBITMQ_OPERATOR
+#argocd app sync $ADAPTATION_SERVICE
+#argocd app sync $ADMINISTRATION_SERVICE
+#argocd app sync $NCFS_SERVICE
+#argocd app sync $MONITORING_SERVICE
+#argocd app sync $MONITORING_SERVICE_GRAFANA
+#argocd app sync $CERT_MANAGER
 
 
 
